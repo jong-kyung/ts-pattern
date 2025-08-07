6bd02612726d76722d10630fafc529e1660acb89
이종경 <gyoung3063413@naver.com>
2025-08-07 23:28:05 +0900

    feat: implement HistoryStack and Cloneable interface for DrawingBoardHistory

--- src/DrawingBoardHistory.ts
+++ src/DrawingBoardHistory.ts
@@ -2,10 +2,29 @@ import type DrawingBoard from "./AbstractDrawingBoard";
 import type ChromeDrawingBoard from "./ChromeDrawingBoard";
 import type IEDrawingBoard from "./IEDrawingBoard";
 
+interface Cloneable {
+  clone(): Cloneable;
+}
+
+class HistoryStack extends Array implements Cloneable {
+  // 프로토타입의 핵심은 clone 메서드, 조건에 따라 깊은 복사 또는 얕은 복사를 구현
+  clone(): Cloneable {
+    return this.slice() as HistoryStack;
+  }
+}
+
 export abstract class DrawingBoardHistory {
   drawingBoard: DrawingBoard;
+  stack: HistoryStack;
+
+  getStack() {
+    return this.stack.clone();
+  }
+
   protected constructor(drawingBoard: DrawingBoard) {
     this.drawingBoard = drawingBoard;
+    // 프로토타입 패턴 (자바스크립트의 프로토타입과는 다름)
+    this.stack = new HistoryStack();
   }
 
   abstract initialize(): void;
